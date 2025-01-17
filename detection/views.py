aa11632497fc5e84fe6811231aa997f3a6413ac0
Shivam Mittal <126662156+shivammittal09@users.noreply.github.com>
2025-01-18 03:01:26 +0530

    Update views.py

--- detection/views.py
+++ detection/views.py
@@ -15,7 +15,7 @@ from django.conf import settings
 import google.generativeai as ai
 
 # Configure the API
-API_KEY = os.environ.get('GOOGLE_API_KEY') 'Your_API_Key'
+API_KEY = os.environ.get('GOOGLE_API_KEY') or 'Your_API_Key'
 ai.configure(api_key=API_KEY)
 model = ai.GenerativeModel("gemini-pro")
 chat = model.start_chat()
