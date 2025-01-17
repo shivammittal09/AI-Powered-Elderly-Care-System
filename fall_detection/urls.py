6ec313e5578bc6aac9a4bd5d3dae0c364efcca6c
Shivam Mittal <126662156+shivammittal09@users.noreply.github.com>
2025-01-18 02:20:59 +0530

    Update urls.py

--- fall_detection/urls.py
+++ fall_detection/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import include,path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 
 urlpatterns = [
+    path('', lambda request: redirect('detection/dashboard', permanent=True)),
     path('admin/', admin.site.urls),
     path('detection/', include('detection.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
